df4df1e73780ab1487a42235a5ebb560aceca101
ℜodrigo ℭacilhας <rodrigo.cacilhas@sieve.com.br>
2016-02-23 11:01:47 -0300

    set status variables directly to $scope instead of controller

--- js/cacilhas.js
+++ js/cacilhas.js
@@ -1,21 +1,23 @@
 angular.module('cacilhasApp', ['ngRoute'])
-.controller('CacilhasController', function() {
+.controller('CacilhasController', function($scope) {
 
-    this.homeActive = true;
-    this.aboutActive = false;
-    this.currentPage = $('<li>');
+    var homeActive = true;
+    var aboutActive = false;
 
     var url = window.location.href.split('?url=')[1];
     url = url || '/home';
 
     if (url === '/about') {
-        this.homeActive = false;
-        this.aboutActive = true;
+        homeActive = false;
+        aboutActive = true;
     } else if (url !== '/home') {
-        this.homeActive = false;
+        homeActive = false;
         $('<a href="' + window.location.href + '">')
             .text(url).appendTo($('#current'));
     }
+
+    $scope.homeActive = homeActive;
+    $scope.aboutActive = aboutActive;
 
     url = '/templates' + url + '.html';
     $('#main').load(url);
