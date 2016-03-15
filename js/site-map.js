46acc5252900a8b59fcb8549082c2549eaed08d9
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-03-15 20:52:47 -0300

    Kodumaro first article

--- js/site-map.js
+++ js/site-map.js
@@ -4,5 +4,11 @@ var siteMap = [
         title: 'Apresentação',
         content: 'Sobre este <em>site</em>.',
         highlight: true
-    }
+    },
+	{
+		url: '?t=/kodumaro/2016/03/sml',
+		title: 'Kodumaro :: Standard ML',
+		content: 'Standard ML é a definição padrão da família de linguagens ML.',
+		highlight: true
+	}
 ];
