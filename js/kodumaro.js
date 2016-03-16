d435789368b2b886a1b23a0f2847034ecc3ed14c
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-03-16 08:39:55 -0300

    take kodumaro background back!

---
+++ js/kodumaro.js
@@ -0,0 +1,9 @@
+document.title = 'Kodumaro';
+$('body').css('background-image', 'url(/img/kodumaro-bg.jpg)')
+		 .css('background-repeat-x', 'repeat')
+		 .css('background-repeat-y', 'repeat')
+		 .css('background-attachment', 'fixed')
+		 .css('background-position-x', 'initial')
+		 .css('background-position-y', 'initial')
+		 .css('background-origin', 'initial')
+		 .css('background-clip', 'initial');
