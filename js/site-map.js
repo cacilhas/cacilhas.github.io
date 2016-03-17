805ffd97e666bfd2a7a60f8bf4d9649394856bc8
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-03-17 10:57:13 -0300

    sml functors

--- js/site-map.js
+++ js/site-map.js
@@ -3,13 +3,19 @@ var siteMap = [
         url: '/?t=/about',
         title: 'Apresentação',
         content: 'Sobre este <em>site</em>.',
-		tags: []
+        tags: []
     },
-	{
-		url: '?t=/kodumaro/2016/03/sml',
-		title: 'Kodumaro :: Standard ML',
-		content: 'Standard ML é a definição padrão da família de linguagens ML.',
-		tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML'],
-		highlight: true
-	}
+    {
+        url: '?t=/kodumaro/2016/03/sml',
+        title: 'Kodumaro :: Standard ML',
+        content: 'Standard ML é a definição padrão da família de linguagens ML.',
+        tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML'],
+        highlight: true
+    },
+    {
+        url: '?t=/kodumaro/2016/03/sml',
+        title: 'Kodumaro :: Standard ML – continuação',
+        content: 'Veja como podemos estender a criação de sequências.',
+        tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML'],
+    }
 ];
