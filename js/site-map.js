ab344f9ec2fd409002e985e1fd58ec54c001bd75
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-04-19 23:00:01 -0300

    sort from last to first

--- js/site-map.js
+++ js/site-map.js
@@ -1,5 +1,19 @@
 var siteMap = [
     {
+        url: '?t=/kodumaro/2016/04/aspectos',
+        title: 'Kodumaro :: Aspectos',
+        content: 'Orientação a Aspectos consiste em encapsular funcionalidades conforme sua importância.',
+        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
+        highlight: true
+    },
+    {
+        url: '?t=/kodumaro/2016/04/aspectos-2',
+        title: 'Kodumaro :: Aspectos – continuação',
+        content: 'Uso de <em>mixins</em> na orientação a aspectos.',
+        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
+        hidden: true
+    },
+    {
         url: '/?t=/about',
         title: 'Apresentação',
         content: 'Sobre este <em>site</em>.',
@@ -17,20 +31,6 @@ var siteMap = [
         title: 'Kodumaro :: Standard ML – continuação',
         content: 'Veja como podemos estender a criação de sequências.',
         tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML'],
-        hidden: true
-    },
-    {
-        url: '?t=/kodumaro/2016/04/aspectos',
-        title: 'Kodumaro :: Aspectos',
-        content: 'Orientação a Aspectos consiste em encapsular funcionalidades conforme sua importância.',
-        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
-        highlight: true
-    },
-    {
-        url: '?t=/kodumaro/2016/04/aspectos-2',
-        title: 'Kodumaro :: Aspectos – continuação',
-        content: 'Uso de <em>mixins</em> na orientação a aspectos.',
-        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
         hidden: true
     }
 ];
