632b938cc6c2cde2b3f9c8e7b82c3ad7912b2534
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-05-15 21:15:02 -0300

    simplify tags

--- js/site-map.js
+++ js/site-map.js
@@ -9,48 +9,48 @@ var siteMap = [
         url: '?t=/montegasppa/2016/05/song-can-write-itself',
         title: 'Reflexões… :: O’Keefe Music Foundation',
         content: 'A song can write itself if you just dig deep…',
-        tags: ['Reflexões… :: Música'],
+        tags: ['Música'],
         highlight: true
     },
     {
         url: '?t=/kodumaro/2016/05/quebra-de-contrato',
         title: 'Kodumaro :: Quebra de Contrato',
         content: 'É muito comum programadores inexperientes desrespeitarem regras estabelecidas…',
-        tags: ['Kodumaro :: Python', 'Kodumaro :: TDD'],
+        tags: ['Python', 'TDD'],
         highlight: true
     },
     {
         url: '?t=/kodumaro/2016/05/moonscript',
         title: 'Kodumaro :: MoonScript sobre LuaJIT',
         content: 'Colocar MoonScript para trabalhar com LuaJIT não é trivial. É preciso uma série de pequenos <em>hacks</em> pra funcionar.',
-        tags: ['Kodumaro :: Lua'],
+        tags: ['Lua'],
         highlight: true
     },
     {
         url: '?t=/kodumaro/2016/04/aspectos',
         title: 'Kodumaro :: Aspectos – parte Ⅰ',
         content: 'Orientação a Aspectos consiste em encapsular funcionalidades conforme sua importância.',
-        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
+        tags: ['Conceitual', 'Python'],
         highlight: true
     },
     {
         url: '?t=/kodumaro/2016/04/aspectos-2',
         title: 'Kodumaro :: Aspectos – parte Ⅱ',
         content: 'Uso de <em>mixins</em> na orientação a aspectos.',
-        tags: ['Kodumaro :: Conceitual', 'Kodumaro :: Python'],
+        tags: ['Conceitual', 'Python'],
         hidden: true
     },
     {
         url: '?t=/kodumaro/2016/03/sml',
         title: 'Kodumaro :: Standard ML',
         content: 'Standard ML é a definição padrão da família de linguagens ML.',
-        tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML']
+        tags: ['Funcional', 'ML']
     },
     {
         url: '?t=/kodumaro/2016/03/sml-2',
         title: 'Kodumaro :: Standard ML – continuação',
         content: 'Veja como podemos estender a criação de sequências.',
-        tags: ['Kodumaro :: Funcional', 'Kodumaro :: ML'],
+        tags: ['Funcional', 'ML'],
         hidden: true
     }
 ];
