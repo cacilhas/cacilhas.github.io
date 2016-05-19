6ea1d8c1660530eaada7a691c11482efcb1b380b
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-05-18 23:19:06 -0300

    fix handleOldURL

--- _harp/js/handle-old-url.js
+++ _harp/js/handle-old-url.js
@@ -1,8 +1,8 @@
 function handleOldURL() {
-  var curPath = window.location.pathname;
-  if (curPath.index("/?t=") == 0) {
-    var url = decodeURIComponent(curPath.substr(4)) + ".html";
-    window.location.pathname = url;
+  var oldPath = window.location.search;
+  if (oldPath.indexOf("?t=/") == 0) {
+    var url = decodeURIComponent(oldPath.substr(3)) + ".html";
+    window.location = url;
   }
 }
 
