d091f79634ca579045ea4c97e845f0480ac0fe62
Arĥimedeς ℳontegasppα ℭacilhας <batalema@cacilhas.info>
2016-05-22 22:22:44 -0300

    add Erlang files for Monty Hall

---
+++ _harp/misc/monty-hall/mh.hrl
@@ -0,0 +1,8 @@
+%%%=============================================================================
+%%% @author Rodrigo Cacilhας <batalema@cacilhas.info>
+%%%         [http://kodumaro.blogspot.com/]
+%%% @copyright ©2016, Rodrigo Cacilhας
+%%% @end
+%%%=============================================================================
+-type prize() :: car | goat.
+-type door() :: a | b | c.
